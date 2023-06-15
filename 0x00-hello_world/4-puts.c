10522c70233f2dbfbe3155ed2653ab9938444f55
louaye100 <louayealami10@gmail.com>
2023-06-15 15:17:53 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,10 @@
-#include <stdio.h>
 /**
- * main - 4-puts.c
- * description - Write a C program that prints exactly
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-betty 4-puts.c
-        return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-
